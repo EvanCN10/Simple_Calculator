bd1b5c626171a33776ab6b2d226596bcf27e3072
EvanCN10 <evan66nainggolan@gmail.com>
2025-02-15 22:47:16 +0700

    Update 1.1 (AC, Input)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // File JS
+
+// Muatan awal untuk menampung jumlah input
+var calc_display = "";
+
+function buttonclick(e) {
+  // Kondisi ketika "AC" ditekan
+  if (e == "AC") {
+    calc_display = "";
+  } else {
+    // Jika tombol nomor ditekan (selain operator aritmatika), maka nomor2 tersebut akan digabung menjadi 1 string
+    calc_display += e;
+    console.log(calc_display);
+  }
+
+  document.getElementById("display").value = calc_display;
+}
